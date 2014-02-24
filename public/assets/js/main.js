62bfbe234042b178129b60eed06d4495478fd0f4
dc2bar <dc2bar@gmail.com>
2014-02-24 10:55:50 -0700

    Initial commit

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -124,7 +124,7 @@ $(function (){
   App.Views.UsersList = Backbone.View.extend({
     el: '.userslist',
     initialize: function () {
-      $.getJSON('/getUsers', function(data){
+      $.getJSON('/getUsers?callback=?', function(data){
         console.log(data);
       })
       usersCollection.add(App.User);
