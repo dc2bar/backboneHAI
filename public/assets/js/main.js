6d7e89e24d405968fab7365337ddb71986ba6816
dc2bar <dc2bar@gmail.com>
2014-02-24 23:15:12 -0700

    Initial commit

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -14,12 +14,8 @@ $(function (){
         var checkinurl = 'stillHere?name='+encodeURI(App.User.get('name'))+'&color='+encodeURI(App.User.get('color'))+'&avatar='+encodeURI(App.User.get('avatar'))+'&title='+encodeURI(App.User.get('title'))+'&callback=?';
         $.getJSON(checkinurl, function(data){
           data.sort(function(a, b){
-            console.log(a.name);
-            console.log(b.name);
-            console.log('-------');
-            return a.name-b.name
+            return b.name-a.name
           })
-          console.log(data);
           var current = JSON.stringify(usersCollection);
           var server = JSON.stringify(data);
           if(current != server) {
