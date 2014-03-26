33b68da3e109d0bd6b8805085d96f20caad2b04d
dc2bar <dc2bar@gmail.com>
2014-03-26 15:45:42 -0600

    Initial commit

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ pubnub.subscribe({
   callback: function (data) {
     if (data.method === 'create') {
       messagesCollection.add(data.model);
-      console.log(data.model);
+      console.log(messagesCollection.length);
     } else if (data.method === 'update') {
       messagesCollection.remove(data.model);
     } else if (data.method === 'delete') {
