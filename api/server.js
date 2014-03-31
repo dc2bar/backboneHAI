e3cfdb072ad5dafcf13ba3fec530ebf606b6fcd3
dc2bar <dc2bar@gmail.com>
2014-03-30 20:28:10 -0600

    Initial commit

---
+++ api/server.js
@@ -0,0 +1,33 @@
+var express = require('express'),
+    messages = require('./routes/messages');
+
+var app = express();
+
+app.get('/messages', messages.findAll);
+app.get('/messages/:id', messages.findById);
+app.get('/messages/catchUp', messages.catchUp);
+
+app.listen(3000);
+console.log('Listening on port 3000...');
+
+
+var express = require('express'),
+    message = require('./routes/messages');
+
+var app = express();
+
+app.configure(function () {
+  app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+  app.use(express.bodyParser());
+});
+
+app.get('/messages', message.findAll);
+app.get('/messages/:id', message.findById);
+app.post('/messages', message.addMessage);
+/* these should never be used in our application
+  app.put('/messages/:id', wine.updateMessage);
+  app.delete('/messages/:id', wine.deleteMessage);
+*/
+
+app.listen(3000);
+console.log('Listening on port 3000...');
